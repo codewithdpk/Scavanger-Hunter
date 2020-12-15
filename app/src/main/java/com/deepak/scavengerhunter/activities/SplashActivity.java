c66572a6101fd7907992256054c279dfb8918c4c
deepak <codewithdeepak@yahoo.com>
2020-12-16 00:35:41 +0530

    First commit

---
+++ app/src/main/java/com/deepak/scavengerhunter/activities/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.deepak.scavengerhunter.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.deepak.scavengerhunter.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
